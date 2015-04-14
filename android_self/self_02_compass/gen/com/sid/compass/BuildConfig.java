ce10a6d9c54a052616c9d276554e9b1ce306bee3
阳 <@sid-pc>
2015-04-14 18:06:57 +0800

    删除

--- android_self/self_02_compass/gen/com/sid/compass/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.sid.compass;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- android_self/self_sendmsg_v1.0/gen/com/send/msg/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.send.msg;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
